f3bfc78be82c5c6711480080d6be27a88aa67233
unknown <thom.orta@gmail.com>
2024-04-11 00:39:51 +0200

    in drawer add gitHub link with lauchUrl widget, modify AdroidManifeste for open https link, add text in drawerheader

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 
 import '../../../routes/app_pages.dart';
 import '../controllers/home_controller.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({super.key});
@@ -15,60 +16,131 @@ class HomeView extends GetView<HomeController> {
         child: ListView(
           children: [
             DrawerHeader(
-              decoration: const BoxDecoration(
-                color: Colors.red,
+              decoration: BoxDecoration(
                 image: DecorationImage(
-                  image: NetworkImage(
-                    'https://plus.unsplash.com/premium_photo-1661877737564-3dfd7282efcb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Zmx1dHRlcnxlbnwwfHwwfHx8MA%3D%3D'),
+                  onError: (error,stackTrace) => const Center(child: CircularProgressIndicator(color: Colors.black,)),
+                  image: const NetworkImage('https://img.freepik.com/free-photo/futurism-perspective-digital-nomads-lifestyle_23-2151252447.jpg?t=st=1712788171~exp=1712791771~hmac=e79df48c098512faa26829cc91b2efeabcf9709a16533f291f7e12e95b8f54a2&w=996'),
                   fit: BoxFit.fill,
                 ),
               ),
-              child: Stack(
-                alignment: Alignment.topRight,
-                children: <Widget>[
-                  Text(
-                    'Thomas ORTA',
-                    textAlign: TextAlign.end,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      foreground: Paint()
-                        ..style = PaintingStyle.stroke
-                        ..strokeWidth = 3
-                        ..color = Colors.amber, // <-- Border color
-                    ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Stack(
+                    alignment: Alignment.topRight,
+                    children: <Widget>[
+                      Text(
+                        'Thomas ORTA',
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          foreground: Paint()
+                            ..style = PaintingStyle.stroke
+                            ..strokeWidth = 2
+                            ..color = Colors.amber, // <-- Border color
+                        ),
+                      ),
+                      const Text(
+                        'Thomas ORTA',
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black, // <-- Inner color
+                        ),
+                      ),
+                    ],
                   ),
-                  const Text(
-                    'Thomas ORTA',
-                    textAlign: TextAlign.end,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white, // <-- Inner color
-                    ),
+                  Stack(
+                    alignment: Alignment.topRight,
+                    children: <Widget>[
+                      Text(
+                        'Développeur',
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          foreground: Paint()
+                            ..style = PaintingStyle.stroke
+                            ..strokeWidth = 2
+                            ..color = Colors.amber, // <-- Border color
+                        ),
+                      ),
+                      const Text(
+                        'Développeur',
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black, // <-- Inner color
+                        ),
+                      ),
+                    ],
+                  ),
+                  Stack(
+                    alignment: Alignment.topRight,
+                    children: <Widget>[
+                      Text(
+                        'full stack',
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          foreground: Paint()
+                            ..style = PaintingStyle.stroke
+                            ..strokeWidth = 2
+                            ..color = Colors.amber, // <-- Border color
+                        ),
+                      ),
+                      const Text(
+                        'full stack',
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black, // <-- Inner color
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
             ),
             ListTile(
-              leading: Icon(Icons.home),
-              title: Text("Home"),
+              leading: const Icon(Icons.home, color: Colors.black,size: 20),
+              title: const Text("Home"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.account_box, color: Colors.black, size: 20),
+              title: const Text("À propos de moi"),
               onTap: () {},
             ),
             ListTile(
-              leading: Icon(Icons.account_box),
-              title: Text("About"),
+              leading: const Icon(Icons.grid_3x3_outlined, color: Colors.black, size: 20,),
+              title: const Text("Mon curriculum vitae "),
               onTap: () {},
             ),
             ListTile(
-              leading: Icon(Icons.grid_3x3_outlined),
-              title: Text("Products"),
+              leading: const Icon(Icons.contact_mail, color: Colors.black, size: 20),
+              title: const Text("Contact"),
               onTap: () {},
             ),
-            ListTile(
-              leading: Icon(Icons.contact_mail),
-              title: Text("Contact"),
-              onTap: () {},
+            InkWell(
+              onTap: () {
+                launchUrl(Uri.parse('https://github.com/Web82-Thom/appbook'));
+              },
+              child: ListTile(
+                leading: Image.asset(
+                  'assets/images/github.png',
+                  width: 20,
+                ),
+                title: const Text("Github Web82-Thom",),
+              ),
             )
           ],
         ),
