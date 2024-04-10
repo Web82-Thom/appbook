2e1110f3899d1eec8974bf7f63f01656d957d590
unknown <thom.orta@gmail.com>
2024-04-10 18:06:12 +0200

    create left drawer

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -7,17 +7,84 @@ import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                color: Colors.red,
+                image: DecorationImage(
+                  image: NetworkImage(
+                    'https://plus.unsplash.com/premium_photo-1661877737564-3dfd7282efcb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Zmx1dHRlcnxlbnwwfHwwfHx8MA%3D%3D'),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              child: Stack(
+                alignment: Alignment.topRight,
+                children: <Widget>[
+                  Text(
+                    'Thomas ORTA',
+                    textAlign: TextAlign.end,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      foreground: Paint()
+                        ..style = PaintingStyle.stroke
+                        ..strokeWidth = 3
+                        ..color = Colors.amber, // <-- Border color
+                    ),
+                  ),
+                  const Text(
+                    'Thomas ORTA',
+                    textAlign: TextAlign.end,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white, // <-- Inner color
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Home"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.account_box),
+              title: Text("About"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.grid_3x3_outlined),
+              title: Text("Products"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.contact_mail),
+              title: Text("Contact"),
+              onTap: () {},
+            )
+          ],
+        ),
+      ),
       appBar: AppBar(
         actions: [
-          IconButton(onPressed: (){Get.toNamed(Routes.WELCOME);}, icon: const Icon(Icons.exit_to_app))
+          IconButton(
+            onPressed: () {
+              Get.toNamed(Routes.WELCOME);
+            },
+            icon: const Icon(Icons.exit_to_app),
+          ),
         ],
         automaticallyImplyLeading: false,
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title:Text(controller.title),
+        title: Text(controller.title),
         centerTitle: true,
       ),
       body: Center(
@@ -25,14 +92,15 @@ class HomeView extends GetView<HomeController> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              textAlign: TextAlign.center, 
+              textAlign: TextAlign.center,
               'You have pushed the button this many times:',
             ),
-            Obx(() =>Text(
-              controller.count.string,
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),),
-            
+            Obx(
+              () => Text(
+                controller.count.string,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ),
           ],
         ),
       ),
@@ -40,24 +108,29 @@ class HomeView extends GetView<HomeController> {
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           FloatingActionButton(
-            onPressed: (){
+            onPressed: () {
               controller.incrementCounter();
-            } ,
+            },
             tooltip: 'Increment',
             heroTag: null,
             child: const Icon(Icons.add),
           ),
-          const SizedBox (height: 5,),
+          const SizedBox(
+            height: 5,
+          ),
           FloatingActionButton(
-            onPressed:(){
+            onPressed: () {
               controller.incrementCounterless();
             },
             tooltip: 'Increment',
             heroTag: null,
-            child: const Text("-", style: TextStyle(fontSize: 30.00),),
+            child: const Text(
+              "-",
+              style: TextStyle(fontSize: 30.00),
+            ),
           ),
         ],
-      )
+      ),
     );
   }
 }
